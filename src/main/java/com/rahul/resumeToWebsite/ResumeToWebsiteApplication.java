6eee0168c747210ac3cbf2d1cf0e79f2d6a1f633
Rahul Verma <rahulverma@Rahuls-MacBook-Air.local>
2024-07-26 21:10:26 +0530

    rahul verma

---
+++ src/main/java/com/rahul/resumeToWebsite/ResumeToWebsiteApplication.java
@@ -0,0 +1,14 @@
+package com.rahul.resumeToWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeToWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeToWebsiteApplication.class, args);
+		System.out.println("Hi! I'm Rahul Verma");
+	}
+
+}
---
+++ src/test/java/com/rahul/resumeToWebsite/ResumeToWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahul.resumeToWebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeToWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
